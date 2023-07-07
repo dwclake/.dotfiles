16766857fa9da9012c2ed47aa9c2cae014eb243d
dwclake <clake.dw@gmail.com>
2023-07-07 10:30:33 -0700

    initial commit

--- endeavouros/.config/nvim/lua/dwclake/packer.lua
+++ endeavouros/.config/nvim/lua/dwclake/packer.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
   })
 
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use ('nvim-treesitter/nvim-treesitter-context')
   use ('nvim-treesitter/playground')
   use ('theprimeagen/harpoon')
   use ('mbbill/undotree')
