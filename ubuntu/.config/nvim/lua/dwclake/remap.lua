2e41261a880db053d80a49f440004e644f960671
dwclake <clake.dw@gmail.com>
2023-08-03 10:31:45 -0700

    initial commit

--- ubuntu/.config/nvim/lua/dwclake/remap.lua
+++ ubuntu/.config/nvim/lua/dwclake/remap.lua
@@ -7,6 +7,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>")
 vim.keymap.set("n", "<leader>t", ":ToggleTerm direction=float<CR>")
 
+vim.keymap.set("n", ".", ":tabnext<CR>")
+vim.keymap.set("n", ",", ":tabprevious<CR>")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
