4e8b3783b0e487eb6fb558e93f0fe99ca7f9a899
dwclake <clake.dw@gmail.com>
2023-09-13 08:45:29 -0700

    initial commit

--- endeavouros/.config/nvim/after/plugin/colors.lua
+++ endeavouros/.config/nvim/after/plugin/colors.lua
@@ -4,11 +4,12 @@ require('rose-pine').setup({
 
 require('kanagawa').setup({
 	keywordStyle = { italic = false },
-    typeStle = { italic = false },
-    functionStyle = { italic = false },
-    statementStyle = { italic = false, bold = false },
-    commentStyle = { italic = true },
-    undercurl = false
+  typeStle = { italic = false },
+  functionStyle = { italic = false },
+  statementStyle = { italic = false, bold = false },
+  commentStyle = { italic = true },
+  undercurl = false,
+  transparent = true
 })
 
 
@@ -19,6 +20,7 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.cmd(":highlight SignColumn guibg=none")	
+    vim.cmd(":highlight LineNr guibg=none")	
     vim.cmd(":highlight GitSignsAdd guibg=none")	
     vim.cmd(":highlight GitSignsDelete guibg=none")	
     vim.cmd(":highlight GitSignsChange guibg=none")	
--- endeavouros/.config/nvim/lua/dwclake/packer.lua
+++ endeavouros/.config/nvim/lua/dwclake/packer.lua
@@ -17,10 +17,7 @@ return require('packer').startup(function(use)
 
   use ({
 	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+	  as = 'rose-pine'
   })
   use ({'rebelot/kanagawa.nvim'})
 
