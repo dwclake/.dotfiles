d037839a76be15a0c598bff70f4b40b62055b057
dwclake <clake.dw@gmail.com>
2023-09-17 21:26:50 -0700

    initial commit

--- ubuntu/.config/nvim/after/plugin/treesitter.lua
+++ ubuntu/.config/nvim/after/plugin/treesitter.lua
@@ -21,7 +21,7 @@ require'nvim-treesitter.configs'.setup {
 }
 
 require'treesitter-context'.setup{
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
     line_numbers = true,
     mode = "cursor",
     throttle = true, -- Throttles plugin updates (may improve performance)
