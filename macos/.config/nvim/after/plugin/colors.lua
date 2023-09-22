021fe880e2b90d80a8efbd3befe4a474c1e49a74
dwclake <devonwebb559@gmail.com>
2023-09-21 21:32:35 -0700

    initial commit

--- macos/.config/nvim/after/plugin/colors.lua
+++ macos/.config/nvim/after/plugin/colors.lua
@@ -2,10 +2,28 @@ require('rose-pine').setup({
 	disable_italics = true
 })
 
+require('nordic').setup({
+  on_palette = function(palette)
+    palette.white0 = '#D8DEE9'
+    palette.red.dim = palette.red.bright
+    palette.red.base = palette.red.bright
+    palette.orange.dim = palette.orange.bright
+    palette.orange.base = palette.orange.bright
+    palette.cyan.dim = palette.cyan.bright
+    palette.cyan.base = palette.cyan.bright
+    palette.cyan.dim = palette.cyan.bright
+    palette.cyan.base = palette.cyan.bright
+    return palette
+  end,
+  bold_keywords = false,
+  italic_comments = false,
+  transparent_bg = true,
+  reduce_blue = true
+})
+
 function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
- 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.cmd(":highlight SignColumn guibg=none")	
@@ -27,4 +45,4 @@ function ColorMyPencils(color)
     vim.cmd(":highlight GitSignsUntrackedLn guibg=none")	
 end
 
-ColorMyPencils("nordic")
+ColorMyPencils("rose-pine")
