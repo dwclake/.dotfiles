6a1862e502b6f5083a75ca663f3ebc7bb5ad76eb
dwclake <clake.dw@gmail.com>
2023-11-22 14:02:34 -0800

    initial commit

--- endeavouros/.config/nvim/lua/dwclake/packer.lua
+++ endeavouros/.config/nvim/lua/dwclake/packer.lua
@@ -40,7 +40,6 @@ return require('packer').startup(function(use)
       })
   end}
   use ('reasonml-editor/vim-reason-plus')
-  use ('alaviss/nim.nvim')
   use ('jlcrochet/vim-crystal')
   use ('kdheepak/lazygit.nvim')
  
