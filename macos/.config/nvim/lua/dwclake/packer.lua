5d108b10fcc46bd61d946bd109370f2fee61f175
dwclake <clake.dw@gmail.com>
2023-12-04 19:28:32 -0800

    initial commit

--- macos/.config/nvim/lua/dwclake/packer.lua
+++ macos/.config/nvim/lua/dwclake/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
   use ('AlexvZyl/nordic.nvim')
 
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use ('nvim-treesitter/nvim-treesitter-context')
   use ('nvim-treesitter/playground')
   use {
       'theprimeagen/harpoon',
