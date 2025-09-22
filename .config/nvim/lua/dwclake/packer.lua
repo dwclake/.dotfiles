c7549d245c4b646e192a2cdbba64c0f690dd666a
dwclake <clake.dw@gmail.com>
2025-09-22 10:26:10 -0700

    initial commit

--- .config/nvim/lua/dwclake/packer.lua
+++ .config/nvim/lua/dwclake/packer.lua
@@ -8,14 +8,12 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
+	  'nvim-telescope/telescope.nvim', branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use ('github/copilot.vim')
   use ('reasonml-editor/vim-reason-plus')
-  use ('jlcrochet/vim-crystal')
   use ('gleam-lang/gleam.vim')
 
   use ({
