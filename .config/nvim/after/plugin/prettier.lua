c25155a5652636ef45fa6cdab3a9b3149aebb5ef
dwclake <clake.dw@gmail.com>
2025-12-01 16:02:23 -0800

    initial commit

--- .config/nvim/after/plugin/prettier.lua
+++ .config/nvim/after/plugin/prettier.lua
@@ -3,4 +3,4 @@ vim.g["prettier#autoformat_config_present"] = 1
 
 vim.g["prettier#tab_width"] = 4
 vim.g["prettier#use_tabs"] = "true"
-vim.g["prettier#print_width"] = "80"
+vim.g["prettier#print_width"] = "90"
