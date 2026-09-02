6ae9d69cf8ce4ead7b8127a427f15c3011457fd0
dwclake <clake.dw@gmail.com>
2026-09-02 08:46:53 -0700

    initial commit

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -62,9 +62,9 @@ cmp.setup({
         ['<Return>'] = cmp.mapping.confirm({ select = false }),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<Tab>'] = cmp.mapping(function(fallback)
-            --if vim.b._copilot_suggestion ~= nil then
-            --    vim.fn.feedkeys(vim.api.nvim_replace_termcodes(vim.fn['copilot#Accept'](), true, true, true), '')
-            if cmp.visible() then
+            if vim.b._copilot_suggestion ~= nil then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes(vim.fn['copilot#Accept'](), true, true, true), '')
+			elseif cmp.visible() then
                 cmp.select_next_item()
 			elseif require('luasnip').expand_or_jumpable() then
                 vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
